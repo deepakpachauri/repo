50ae47a41d3e6acd32f83158becbd3219ae046e9
deepakpachauri <deepakpachauri@gmail.com>
2017-02-28 22:36:00 +0100

    StringUtility

--- StringUtilities/src/test/java/com/pachauri/utilities/StringUtilityTest.java
+++ StringUtilities/src/test/java/com/pachauri/utilities/StringUtilityTest.java
@@ -5,11 +5,40 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class StringUtilityTest {
-	
+
 	@Test
-	public void isCharDuplicateFalse(){
+	public void isCharDuplicateFalse() {
 		boolean result = StringUtility.isCharDuplicate("abcdefghij");
 		assertEquals(false, result);
 	}
 
+	@Test
+	public void isCharDuplicateTrue() {
+		boolean result = StringUtility.isCharDuplicate("abcdefghijf");
+		assertEquals(true, result);
+	}
+
+	@Test
+	public void isCharDuplicateNull() {
+		boolean result = StringUtility.isCharDuplicate(null);
+		assertEquals(false, result);
+	}
+
+	@Test
+	public void isCharDuplicateBlankString() {
+		boolean result = StringUtility.isCharDuplicate("  ");
+		assertEquals(false, result);
+	}
+
+	@Test
+	public void isCharDuplicateFalseWithSpaces() {
+		boolean result = StringUtility.isCharDuplicate("abcd efgh ij");
+		assertEquals(false, result);
+	}
+
+	@Test
+	public void isCharDuplicateTrueWithSpaces() {
+		boolean result = StringUtility.isCharDuplicate("abcd efgh ij fg");
+		assertEquals(true, result);
+	}
 }
